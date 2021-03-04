77e8b9f3ad55afc3b297adcaaccd7a9588722794
Dave <dave@manjaro.mshome.net>
2021-03-04 16:08:59 +0300

    accept a valid url and  fetch the data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3001
-
+const axios = require('axios')
 app.get('/',(req,res)=>{
     res.send('test')
 })
@@ -9,12 +9,24 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
     next();
   });
-app.get('/',(req,res)=>{
-    const u = req.query["u"];
-  
-    const url = `https://cross-pass.herokuapp.com/${u}`;
+// app.get('/',(req,res)=>{
+//     const u = req.query["u"];
+//   console.log(req);
+//     // const url = `https://cross-pass.herokuapp.com/${u}`;
 
-    request(url).pipe(res);
+//     // request(url).pipe(res);
+// })
+app.get(':endpoint([\\/\\w\\.-]*)', function(req,res){
+
+    let url = req.params.endpoint
+    url = url.substring(1)
+    console.log(url);
+    axios.get(url).then(response =>{
+        res.json(response.data)
+    }).catch(error=>{
+        res.json(error)
+    })
+
 })
-
+//https://api.deezer.com/chart
 app.listen(port ,()=>console.log(`http://localhost:${port}`))
